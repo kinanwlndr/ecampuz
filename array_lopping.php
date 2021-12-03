6bee02887fa21388b39b45fa8e65f73a77b4bee2
kinanwlndr <65544787+kinanwlndr@users.noreply.github.com>
2021-12-03 14:36:00 +0700

    nomer 4

---
+++ array_lopping.php
@@ -0,0 +1,13 @@
+<?php
+$aplikasi[1]= 'gtAkademik';
+$aplikasi[2]= 'gtFinansi';
+$aplikasi[3]= 'gtPerizinan';
+$aplikasi[4]= 'eCampuz';
+$aplikasi[5]= 'eOviz';
+
+$aplikasi2=1;
+while ($aplikasi2 <= count($aplikasi)){
+    echo "Aplikasi : $aplikasi[$aplikasi2]. <br>";
+    $aplikasi2++;
+}
+?>
